cf681cebe9283ae905be2172cc931393ced956c0
Omar Khaled <o.khaled6789@gmail.com>
2023-06-06 05:27:53 +0300

    added 10-check_cycle.c

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * check_cycle - checks if the linked list contains a cycle or not
+ * @list: the head of the list
+ * Return: 0 if there's no cyle, 1 otherwise
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *ptr1, *ptr2;
+
+	ptr1 = list;
+	ptr2 = list;
+	while (ptr2->next != NULL)
+	{
+		ptr1 = ptr1->next;
+		ptr2 = ptr2->next->next;
+		if (ptr2 == NULL)
+			return (0);
+		if (ptr1 == ptr2)
+			return (1);
+	}
+	return (0);
+}
