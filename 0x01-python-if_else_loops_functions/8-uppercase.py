10b93c6125a440e8153994c44032a7967e053092
Omar Khaled <o.khaled6789@gmail.com>
2023-06-08 04:58:48 +0300

    edited identation problem

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,6 +2,6 @@
 def uppercase(str):
     for i in str:
         if ord(i) >= ord('a') and ord(i) <= ord('z'):
-            i = chr(ord(i) - ord('a') + ord('A'))
-            print("{}".format(i), end="")
+            i = chr(ord(i)-ord('a')+ord('A'))
+        print("{}".format(i), end="")
     print()
