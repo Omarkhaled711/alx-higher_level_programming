a8ac22d44f1864689ff1465b1a6fb85fe5e1d9be
Omar Khaled <o.khaled6789@gmail.com>
2023-07-04 04:37:58 +0300

    edited a function prototype to match the requirements

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -15,13 +15,13 @@ class Rectangle:
         return self.__width
 
     @width.setter
-    def width(self, width):
+    def width(self, value):
         """ setter for the width attribute """
-        if not isinstance(width, int):
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        if width < 0:
+        if value < 0:
             raise ValueError("widht must be >= 0")
-        self.__width = width
+        self.__width = value
 
     @property
     def height(self):
@@ -29,10 +29,10 @@ class Rectangle:
         return self.__height
 
     @height.setter
-    def height(self, height):
+    def height(self, value):
         """ setter for the height attribute """
-        if not isinstance(height, int):
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        if height < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
-        self.__height = height
+        self.__height = value
