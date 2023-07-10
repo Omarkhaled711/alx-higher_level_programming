7d3ee0ce4a9c6fbfa050a09ba05353a5890ba8f5
Omar Khaled <o.khaled6789@gmail.com>
2023-07-11 01:35:38 +0300

    updated the test cases, and the doc comments in 1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
 """
-This module contains a class "MyList" that inherits from list
+This module is a part of Alx python inheritance tasks.
+It contains a class "MyList" that inherits from list,
+and a public method called print_sorted()
 """
 
 
 class MyList(list):
     """
-    This class inherits from list class in python
+    This class inherits from list class in python.
+    It contains a public method called print_sorted.
+    creating an object of it:
+    my_list = MyList()
     """
     def print_sorted(self):
         """
