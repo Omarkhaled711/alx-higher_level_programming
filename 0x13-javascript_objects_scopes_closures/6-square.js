43985b8c080a7671e855cbd6ea4631439b014ecc
Omar Khaled <o.khaled6789@gmail.com>
2023-09-13 00:04:28 +0300

    added 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+
+module.exports = class square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+    this.size = size;
+  }
+
+  charPrint (c) {
+    if (c === '' || c === null || c === undefined) {
+      c = 'X';
+    }
+    c = String(c);
+    for (let i = 0; i < this.size; i += 1) {
+      console.log(c.repeat(this.size));
+    }
+  }
+};
